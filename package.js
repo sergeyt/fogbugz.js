42472579cd31399c259d503ddc5b41a9319e1752
sergeyt <stodyshev@gmail.com>
2014-09-22 21:51:41 +0700

    migration to METEOR@0.9 (#3)

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
-  summary: "FogBugz client"
+  name: "sergeyt:fogbugz",
+  summary: "FogBugz client for nodejs, meteor and browser.",
+  git: "https://github.com/sergeyt/fogbugz.js",
+  version: "0.0.32"
 });
 
 Npm.depends({
@@ -10,12 +13,13 @@ Npm.depends({
 	"async": "0.2.9"
 });
 
-Package.on_use(function(api) {
-  api.use('underscore', ['client', 'server']);
-  api.use('jquery', ['client']);
-  api.use('jquery-xml2json', ['client']);
-
+Package.onUse(function(api) {
+  var anywhere = ['client', 'server'];
+  var client = ['client'];
+  api.use('underscore', anywhere);
+  api.use('jquery', client);
+  api.use('jquery-xml2json', client);
   api.export('FogBugz');
-
-  api.add_files('fogbugz.js', ['server', 'client']);
+  api.addFiles('fogbugz.js', anywhere);
 });
+
